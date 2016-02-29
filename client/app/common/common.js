dc834a772b571ee7ff09070c4795de47fe9f7802
Wouter <tuvokki@gmail.com>
2016-02-29 23:23:51 +0100

    Use the footer and header

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -1,11 +1,15 @@
 import angular from 'angular';
 import Navbar from './navbar/navbar';
+import Header from './header/header';
 import Hero from './hero/hero';
+import Footer from './footer/footer';
 import User from './user/user';
 
 let commonModule = angular.module('app.common', [
 	Navbar.name,
-	Hero.name,
+  Header.name,
+  Footer.name,
+  Hero.name,
 	User.name
 ]);
 
