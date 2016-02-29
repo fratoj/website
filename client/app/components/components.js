5b143d7546f28088e91cf209f3d776fdbd3d5a79
Wouter <tuvokki@gmail.com>
2016-02-29 09:29:29 +0100

    Use the new components

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,10 +1,16 @@
 import angular from 'angular';
 import Home from './home/home';
 import About from './about/about'; 
+import Clients from './clients/clients'; 
+import Careers from './careers/careers'; 
+import Contact from './contact/contact'; 
 
 let componentModule = angular.module('app.components', [
 	Home.name,
-	About.name
+	Clients.name,
+  About.name,
+  Careers.name,
+  Contact.name
 ]);
 
 export default componentModule;
