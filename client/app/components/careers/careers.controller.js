78f56cff8384238b4979308ea794a851534e2b7b
Wouter <tuvokki@gmail.com>
2016-03-01 15:52:50 +0100

    Wire up the job listing

--- client/app/components/careers/careers.controller.js
+++ client/app/components/careers/careers.controller.js
@@ -1,7 +1,10 @@
 class CareersController {
-	constructor(){
-		this.name = 'careers';
-	}
+  constructor($firebaseArray){
+    this.name = 'careers';
+    var ref = new Firebase("https://fratoj.firebaseio.com/jobs");
+    // download the data into a local object
+    this.jobs = $firebaseArray(ref);
+  }
 }
 
 
