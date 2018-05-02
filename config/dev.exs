216f589b8136f11605a7e29d5ec69c832c475f77
Wouter <tuvokki@gmail.com>
2018-05-02 16:20:10 +0200

    Add standard angular frontend to Phoenix

--- config/dev.exs
+++ config/dev.exs
@@ -11,7 +11,7 @@ config :blog, BlogWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: []
+  watchers: [ng: ["build", "--watch"]]
 
 # ## SSL Support
 #
