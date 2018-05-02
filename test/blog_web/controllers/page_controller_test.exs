d3a64b3d0279f81378f20ce96a53ecd0fbf21b7a
Wouter <tuvokki@gmail.com>
2018-05-02 16:41:28 +0200

    Fix test

--- test/blog_web/controllers/page_controller_test.exs
+++ test/blog_web/controllers/page_controller_test.exs
@@ -2,7 +2,7 @@ defmodule BlogWeb.PageControllerTest do
   use BlogWeb.ConnCase
 
   test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    conn = get(conn, "/")
+    assert html_response(conn, 200) =~ "Welcome to Fratoj!"
   end
 end
